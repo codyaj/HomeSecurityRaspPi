eb4c4286fa2d1bbbe980673cce87585d4cd397c8
Cody Jackson <57662320+codyaj@users.noreply.github.com>
2023-12-01 00:01:06 +1000

    Update main.py

--- server/main.py
+++ server/main.py
@@ -2,8 +2,9 @@ from sftpCollector import saveFiles
 from disposal import deleteOldVideos
 import time
 
-print(deleteOldVideos()) # Only needs to be checked once per boot | If you're running this on a server with a very high ontime I strongly suggest putting this with the saveFiles function call
+print(deleteOldVideos()) # Only needs to be checked once per boot but if on running on a server with alot of uptime it should be moved into the while loop
 
 while (True):
     print(saveFiles())
+    # Check the response from saveFiles() and if it isnt good sftp a file to central pi to sound a quick alarm (Buzz or something every now and then to alert home owner)
     time.sleep(10800) # Time is in seconds | 10800s = 3h | Therefor Files if present will be saved on pc boot or every 3 hours
