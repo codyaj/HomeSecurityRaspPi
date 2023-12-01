0bd3be64158e0bb0d2979d11273bc0b2819cb851
Cody Jackson <57662320+codyaj@users.noreply.github.com>
2023-12-01 23:37:48 +1000

    Update main.py
    Error handling

--- server/main.py
+++ server/main.py
@@ -2,9 +2,14 @@ from sftpCollector import saveFiles
 from disposal import deleteOldVideos
 import time
 
-print(deleteOldVideos()) # Only needs to be checked once per boot but if on running on a server with alot of uptime it should be moved into the while loop
+print(deleteOldVideos()) # Only needs to be checked once per boot
 
 while (True):
-    print(saveFiles())
+    output = saveFiles()
+    print(output)
+    if (output.split()[0] != "No" or output.split()[0] != "Successful"):
+        break;
     # Check the response from saveFiles() and if it isnt good sftp a file to central pi to sound a quick alarm (Buzz or something every now and then to alert home owner)
     time.sleep(10800) # Time is in seconds | 10800s = 3h | Therefor Files if present will be saved on pc boot or every 3 hours
+
+input()
