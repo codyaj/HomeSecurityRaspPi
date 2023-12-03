6e515010f45292d607be63efb1a12cd5681fafe9
Cody Jackson <57662320+codyaj@users.noreply.github.com>
2023-12-03 20:59:19 +1000

    Update main.py
    Changed logic so the video lengths are not constant.

--- cameraPi/main.py
+++ cameraPi/main.py
@@ -1,25 +1,46 @@
 import picamera
 import datetime
 from time import sleep
+import datetime
 from gpiozero import MotionSensor
 
 pir = MotionSensor(27)
-
-recordingLength = 60*5 # 5 minutes
-
-# init cam
-camera = picamera.PiCamera()
-camera.resolution = (1920, 1080)
-
-time.sleep(2) # sleep for 2 seconds to init cam
+recording = False
+waitingForMotion = False
+timeSinceMotion = 0
+diffSinceMotion = 0
 
 while (True):
-    pir.wait_for_motion()
+    sleep(0.1) # wait
 
-    # Create a file to let the central rasp pi know that motion has been detected
-    x = open("alert.secure", "w")
-    x.close()
-    
-    camera.start_recording(f"video/{datetime.datetime.now().day}_{datetime.datetime.now().month}_{datetime.datetime.now().year}__{datetime.datetime.now().hour}_{datetime.datetime.now().minute}_{datetime.datetime.now().second}.h264")
-    camera.wait_recording(recordingLength)
-    camera.stop_recording()
+    if (waitingForMotion):
+
+        if(pir.motion_detected == True): # Reset the counter
+            timeSinceMotion = datetime.datetime.now()
+
+        diffSinceMotion = (datetime.datetime.now() - timeSinceMotion).total_seconds()
+
+        if (diffSinceMotion <= 20): # wait 20 seconds on waitingForMotion before stopping the recording
+            continue
+
+        else: # Stop the recording and reset variables
+            recording = False
+            waitingForMotion = False
+            timeSinceMotion = 0
+            diffSinceMotion = 0
+            camera.stop_recording()
+            camera.close()
+
+    elif (pir.motion_detected == True and recording == False): # Begin Recording
+        # Init Cam
+        camera = picamera.PiCamera()
+        camera.resolution = (1920, 1080)
+
+        timeSinceMotion = datetime.datetime.now()
+        waitingForMotion = True
+
+        camera.start_recording(f"video/{datetime.datetime.now().day}_{datetime.datetime.now().month}_{datetime.datetime.now().year}__{datetime.datetime.now().hour}_{datetime.datetime.now().minute}_{datetime.datetime.now().second}.h264")
+
+        # Create a file to let the central rasp pi know that motion has been detected
+        x = open("alert.secure", "w")
+        x.close()
